ef981a8e22e7a66590cd5328fbdf48a0520065c8
Daniel Gross <dan.12sq@gmail.com>
2019-12-01 05:07:48 +0000

    day 1

--- 1.py
+++ 1.py
@@ -8,7 +8,12 @@ with open('1', 'r') as f:
 p1 = 0
 p2 = 0
 
-
+for i in inp:
+    p1 += int(i / 3) - 2
+    while i > 0:
+        i = int(i / 3) - 2
+        if i > 0:
+            p2 += i
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
