59d42a073381bc56674f4d694b23a655c37dd8fa
Daniel Gross <dan.12sq@gmail.com>
2019-12-01 04:49:06 +0000

    framework

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.6
+
+inp = []
+with open('1', 'r') as f:
+    for line in f:
+        inp.append(int(line))
+
+p1 = 0
+p2 = 0
+
+
+
+print(f'Part 1: {p1}')
+print(f'Part 2: {p2}')
