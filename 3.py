f2788331086d11db3cc558e9702a596c2b2718e2
Daniel Gross <dan.12sq@gmail.com>
2019-12-03 05:30:43 +0000

    day 3 refactored

--- 3.py
+++ 3.py
@@ -2,52 +2,40 @@
 
 inp = []
 with open('3', 'r') as f:
-    for line in f:
-        inp.append(line.split(','))
+    inp1 = f.readline().split(',')
+    inp2 = f.readline().split(',')
 
 p1 = 0
 p2 = 0
 
-wr = 0
 steps = {}
-for wire in inp:
+
+
+def parse(dirs):
+    global steps
     x, y = 0, 0
     s = 0
-    t = set()
-    for d in wire:
-        dr = d[0]
-        ln = int(d[1:])
-        for i in range(ln):
+    wire = set()
+    for move in dirs:
+        direction = move[0]
+        length = int(move[1:])
+        for i in range(length):
             s += 1
-            if dr == 'R':
-                x += 1
-            elif dr == 'L':
-                x -= 1
-            elif dr == 'U':
-                y += 1
-            else:
-                y -= 1
-            t.add((x, y))
+            x += {'R': 1, 'L': -1}.get(direction, 0)
+            y += {'U': 1, 'D': -1}.get(direction, 0)
+            wire.add((x, y))
             steps[(x, y)] = s + steps.get((x,y), 0)
-    if wr == 0:
-        wr = 1
-        w1 = t
-    else:
-        w2 = t
-        break
+    return wire
 
-ins = []
+
+w1 = parse(inp1)
+w2 = parse(inp2)
+
 for point in w1:
     if point in w2:
-        ins.append(point)
+        md = abs(point[0]) + abs(point[1])
+        p1 = md if p1 == 0 or md < p1 else p1
         p2 = steps[point] if p2 == 0 or steps[point] < p2 else p2
-
-md = []
-for p in ins:
-    x, y = p
-    md.append(abs(x) + abs(y))
-
-p1 = min(md)
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
