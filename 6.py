5f0ab8ce6c2ccb1f21db8673813f574cebd79f72
Daniel Gross <dan.12sq@gmail.com>
2019-12-06 06:23:24 +0000

    day 6 vizualization

--- 6.py
+++ 6.py
@@ -46,6 +46,29 @@ while parent(san[0]):
 for i in range(len(you)):
     if you[i] in san:
         p2 = len(you) + len(san) - 2 - i - san.index(you[i])
+        p2_index = i
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
+
+# vizualize
+you = you[p2_index:]
+san = san[san.index(you[0]):]
+you_hops = '->'.join(you)
+san_hops = '->'.join(san)
+with open('dot6', 'w') as f:
+    f.write('digraph ORBITS {\n')
+    f.write('    node[color=none; shape=plaintext];\n')
+    f.write('    nodesep=0.1; \n')
+    f.write('    sep=0.1; \n')
+    f.write('    edge[weight=1];\n')
+    for inner, outers in orbits.items():
+        for outer in outers:
+            path = '{}->{}'.format(inner, outer)
+            color = 'red' if path in you_hops or path in san_hops or outer in {'YOU', 'SAN'} else 'black'
+            line = '    "{}" -> "{}" [ color={} ];\n'.format(inner, outer, color)
+            f.write(line)
+    f.write('    YOU [color=red,style=filled];\n')
+    f.write('    SAN [color=red,style=filled];\n')
+    f.write('}')
+print('Run `dot -Tsvg -odot6.svg dot6 -v -Kdot`')
