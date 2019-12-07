108447f5aa3c3478a41bb8ad2095b8b284e8c7e6
Daniel Gross <dan.12sq@gmail.com>
2019-12-07 04:34:08 -0500

    day 7 visualization

--- 7.py
+++ 7.py
@@ -69,10 +69,15 @@ def run(reg, i, i_value):
 
 for p in permutations([0, 1, 2, 3, 4]):
     amp = 0
+    amps = []
     for phase in p:
         reg = copy(inp)
         amp = run(reg, 0, [phase, amp])[-1]
+        amps.append(amp)
     p1 = max(p1, amp)
+    if p1 == amp:
+        p1_perm = tuple(map(str, p))
+        p1_amps = tuple(map(str, amps))
 
 for p in permutations([5, 6, 7, 8, 9]):
     regs = []
@@ -82,12 +87,58 @@ for p in permutations([5, 6, 7, 8, 9]):
         inps.append([p[x]])
     ips = [0] * 5
     amp = 0
+    amps = []
     while amp is not None:
         for x in range(5):
             inps[x].append(amp)
             regs[x], ips[x], inps[x], amp = run(regs[x], ips[x], inps[x])
         last_amp = last_amp if amp is None else amp
+        amps.append(amp)
     p2 = max(p2, last_amp)
+    if p2 == last_amp:
+        p2_perm = tuple(map(str, p))
+        p2_amps = tuple(map(str, amps[:-1]))
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
+
+# visualization
+print()
+print('Part 1')
+print()
+l = len(str(max(map(int, p1_amps))))
+header_fmt = ' {} {:' + str(l+1) + '}'
+header = '  '
+line = '0->'
+for i in range(len(p1_amps)):
+    header += header_fmt.format('ABCDE'[i], '(' + p1_perm[i] + ')')
+    line += p1_amps[i]
+    if i != len(p1_perm) - 1:
+        line += ('-'*(l+3 - len(p1_amps[i]))) + '>'
+print(header)
+print(line)
+print()
+print()
+
+print('Part 2')
+print()
+l = len(str(max(map(int, p2_amps))))
+header = '  '
+for i in range(len(p2_amps)):
+    if i < len(p2_perm):
+        header += header_fmt.format('ABCDE'[i], '(' + p2_perm[i] + ')')
+    elif i == len(p2_perm):
+        print(header)
+
+    if i == 0:
+        line = '0->'
+    elif i % len(p2_perm) == 0:
+        print(line)
+        print('   .' + '-' * (len(line) - 4 - len(p2_amps[i-1])) + "'")
+        print('   v')
+        line = '   '
+    line += p2_amps[i]
+    if i % len(p2_perm) != len(p2_perm) - 1:
+        line += ('-'*(l+1 - len(p2_amps[i]))) + '>'
+print(line)
+print()
