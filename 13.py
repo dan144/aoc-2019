f140d807ea0a0710b89f8d92e554882c01b8c2f5
Daniel Gross <dan.12sq@gmail.com>
2019-12-13 00:35:05 -0500

    day 13 refactor: input restored & manipulated automatically

--- 13.py
+++ 13.py
@@ -6,51 +6,30 @@ inp = []
 with open('13', 'r') as f:
     inp = list(map(int, f.readline().split(',')))
 
+for i in range(len(inp)-2):
+    if inp[i] == 0 and inp[i+1] == 3 and inp[i+2] == 0:
+        ball = i + 1
+        break
+
 p1 = 0
 p2 = 0
 
-p = set()
 comp = Intcode(inp)
-xs = set()
-ys = set()
-comp.reg[2122] = 1
 while not comp.done:
-    x = comp.run_until_output()
-    y = comp.run_until_output()
-    t = comp.run_until_output()
-    xs.add(x)
-    ys.add(y)
-    p.add((x, y, t))
-    if t == 2:
-        p1 += 1
+    comp.run_until_output()
+    comp.run_until_output()
+    p1 += comp.run_until_output() == 2
 
 print(f'Part 1: {p1}')
 
-mn_x = min(xs)
-mx_x = max(xs)
-mn_y = min(ys)
-mx_y = max(ys)
-board = []
-for y in range(mx_y+1):
-    board.append([0] * (mx_x+1))
-for x, y, t in p:
-    board[y][x] = t
-
-def disp():
-    for line in board:
-        print(''.join(map(str, line)))
-
-N = 0
-L = -1
-R = 1
 comp = Intcode(inp)
 comp.reg[0] = 2
-comp.reg[2120] = 3
+for r in range(ball - 17, ball + 18):
+    comp.reg[r] = 1 # fake wall
 while not comp.done:
     comp.inputs.append(0)
-    x = comp.run_until_output()
-    y = comp.run_until_output()
-    z = comp.run_until_output()
-p2 = z
+    comp.run_until_output()
+    comp.run_until_output()
+    p2 = comp.run_until_output()
 
 print(f'Part 2: {p2}')
