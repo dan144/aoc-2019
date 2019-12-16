85dc673488ddab13d001ca368486f8214ce39a11
Daniel Gross <dan.12sq@gmail.com>
2019-12-16 10:35:01 -0500

    day 16 part 1 refactor

--- 16.py
+++ 16.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
 
-from intcode import Intcode
+import sys
+
+from copy import copy
 
 inp = []
 with open('16', 'r') as f:
-    inp = list(f.readline().strip())
+    inp = list(map(int, list(f.readline().strip())))
 
 p1 = 0
 p2 = 0
 
 base = [0, 1, 0, -1]
-for phase in range(100):
-    print(phase)
-    n_pattern = []
-    for elem in range(len(inp)):
-        pattern = []
-        for i in range(len(base)):
-            pattern.extend([base[i]] * (elem+1))
-        #pattern.pop(0)
-        #print(pattern)
-        value = 0
-        for j in range(len(inp)):
-            #print(f'{int(inp[j])} * {pattern[(j+1) % len(pattern)]}')
-            value += int(inp[j]) * pattern[(j+1) % len(pattern)]
-        n_pattern.append(abs(value) % 10)
-    inp = list(map(str, n_pattern))
-    #print(''.join(inp))
-    #input()
 
-p1 = ''.join(inp[:8])
+signal = copy(inp)
+items = len(signal)
+for phase in range(100):
+    sys.stdout.write(f'\rPhase {phase}')
+    n_pattern = []
+    for elem in range(items):
+        value = 0
+        for j in range(items):
+            value += int(signal[j]) * base[((j+1)//(elem+1)) % len(base)]
+        n_pattern.append(abs(value) % 10)
+    signal = n_pattern
+
+print()
+p1 = ''.join(map(str, signal[:8]))
 print(f'Part 1: {p1}')
 
 
