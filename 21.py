7e9a7fc04eaae9b2e11faa07d2d50f01798ca1a6
Daniel Gross <dan.12sq@gmail.com>
2019-12-21 12:25:50 -0500

    day 21 part 2

--- 21.py
+++ 21.py
@@ -10,13 +10,14 @@ p1 = 0
 p2 = 0
 
 NL = '\n'
-INS = [
-    'NOT A J',
-    'NOT B T',
-    'OR T J',
-    'NOT C T',
-    'OR T J',
-    'AND D J',
+
+INS = [ # T=? J=?
+    'NOT A J', # J=~A
+    'NOT B T', # T=~B
+    'OR T J',  # J=~A+~B
+    'NOT C T', # T=~C
+    'OR T J',  # J=~A+~B+~C
+    'AND D J', # J=(~A+~B+~C)D
 ]
 cmd_str = NL.join(INS) + NL + 'WALK' + NL
 cmds = list(map(ord, cmd_str))
@@ -33,6 +34,24 @@ print(o)
 
 print(f'Part 1: {p1}')
 
+RUN_INS = [
+    'NOT E T', # T=~E
+    'NOT T T', # T=E
+    'OR H T',  # T=E+H
+    'AND T J', # (~A+~B+~C)D(E+H)
+]
 
+cmd_str = NL.join(INS + RUN_INS) + NL + 'RUN' + NL
+cmds = list(map(ord, cmd_str))
+
+comp = Intcode(inp, cmds)
+o = ''
+while not comp.done:
+    c = comp.run_until_output()
+    if c > 255:
+        p2 = c
+    else:
+        o += chr(c)
+print(o)
 
 print(f'Part 2: {p2}')
