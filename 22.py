8de2fc919c603891b4c8b327a69e66a2adf1a477
Daniel Gross <dan.12sq@gmail.com>
2019-12-22 09:42:54 -0500

    day 22 refactor

--- 22.py
+++ 22.py
@@ -19,11 +19,11 @@ def cut(inc, off, N):
     off += inc * N
     return inc, off
 
-def deal_with_inc(inc, off, N):
-    inc *= pow(N, CARDS-2, CARDS)
+def deal_with_inc(inc, off, N, cards):
+    inc *= pow(N, cards-2, cards)
     return inc, off
 
-def shuffle():
+def shuffle(cards):
     inc = 1
     off = 0
     for line in inp:
@@ -32,27 +32,32 @@ def shuffle():
         elif line.startswith('cut'):
             inc, off = cut(inc, off, int(line.split()[-1]))
         elif line.startswith('deal with'):
-            inc, off = deal_with_inc(inc, off, int(line.split()[-1]))
+            inc, off = deal_with_inc(inc, off, int(line.split()[-1]), cards)
     return inc, off
 
+def f_vals(inc, off, iters, cards):
+    off = off * (1 - pow(inc, iters, cards)) * pow(1 - inc, cards-2, cards)
+    inc = pow(inc, iters, cards)
+    return inc, off
+
+def nth_card(inc, off, n, cards):
+    return (off + n * inc) % cards
+
 CARDS = 10007
-inc, off = shuffle()
-off = off * (1 - pow(inc, 1, CARDS)) * pow(1 - inc, CARDS-2, CARDS)
-inc = pow(inc, 1, CARDS)
+SHUFFLES = 1
+inc, off = shuffle(CARDS)
+inc, off = f_vals(inc, off, SHUFFLES, CARDS)
 
 for i in range(CARDS):
-    if (off + i * inc) % CARDS == 2019:
+    if nth_card(inc, off, i, CARDS) == 2019:
         p1 = i
         break
-
 print(f'Part 1: {p1}')
 
 CARDS = 119315717514047
 SHUFFLES = 101741582076661
+inc, off = shuffle(CARDS)
+inc, off = f_vals(inc, off, SHUFFLES, CARDS)
 
-inc, off = shuffle()
-off = off * (1 - pow(inc, SHUFFLES, CARDS)) * pow(1 - inc, CARDS-2, CARDS)
-inc = pow(inc, SHUFFLES, CARDS)
-
-p2 = (off + 2020 * inc) % CARDS
+p2 = nth_card(inc, off, 2020, CARDS)
 print(f'Part 2: {p2}')
