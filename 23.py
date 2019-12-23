c5840ebda1ef5fd0adb94af4785df6e10812751e
Daniel Gross <dan.12sq@gmail.com>
2019-12-23 01:40:46 -0500

    day 23 visualization

--- 23.py
+++ 23.py
@@ -13,14 +13,17 @@ comps = []
 for c in range(50):
     comps.append(Intcode(inp, [c, -1]))
 
+sent = {}
 NAT = [0, 0]
 dst = 0
 idled = set()
 last_y = -1
 while not p2:
     try:
+        f = dst
         dst, x, y = comps[dst].run_until_n_output(3)
         idled = set()
+        sent[f, dst] = sent.get((f, dst), 0) + 1
         if dst == 255:
             if p1 == 0:
                 p1 = y
@@ -35,9 +38,18 @@ while not p2:
             if last_y == NAT[1]:
                 p2 = last_y
             comps[0].inputs.extend(NAT)
+            sent[255, 0] = sent.get((255, 0), 0) + 1
             last_y = NAT[1]
         dst += 1
         dst = dst % 50
 
 print(f'Part 1: {p1}')
 print(f'Part 2: {p2}')
+
+with open('viz23', 'w') as f:
+    f.write('digraph PACKETS {\n')
+    for (fr, t), n in sent.items():
+        line = '    "{}" -> "{}" [ label="{}" ];\n'.format(fr, t, n)
+        f.write(line)
+    f.write('}')
+print('Run `circo -Tsvg -oviz23.svg viz23 -v`')
